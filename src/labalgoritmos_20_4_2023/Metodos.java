7bc72266f0408b6765a266770a5915bb2746c3e2
jesusCS2000 <castillosalazarjesus30@gmail.com>
2023-04-25 16:01:16 -0600

    Cambio 25\dsad

--- src/labalgoritmos_20_4_2023/Metodos.java
+++ src/labalgoritmos_20_4_2023/Metodos.java
@@ -14,7 +14,7 @@ public class Metodos extends Estudiante{
     public Metodos(String nombre, String ID, String telefono, char sexo, int edad) {
         super(nombre, ID, telefono, sexo, edad);
     }
-    
+    //prueba
     
    public StringBuilder menuImprimir(){
        StringBuilder sB = new
